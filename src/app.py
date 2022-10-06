3a76ab8b89bb89d7936e1be4615b0c39d346c5bb
Molnár Botond <45335158+Gepsonka@users.noreply.github.com>
2022-10-06 19:50:34 +0200

    added token refresh

--- src/app.py
+++ src/app.py
@@ -1,9 +1,10 @@
+from datetime import timedelta
 from flask import Flask, jsonify
 from dotenv import load_dotenv
 import os
 from database.database import db
 from flask_migrate import Migrate
-from flask_jwt_extended import JWTManager
+from flask_jwt_extended import JWTManager, jwt_required, get_jwt_identity
 from auth import auth
 from register import register
 from post import post
@@ -14,6 +15,8 @@ app = Flask(__name__)
 
 app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY')
 app.config['JWT_SECRET_KEY'] = os.environ.get('SECRET_KEY')
+app.config["JWT_ACCESS_TOKEN_EXPIRES"] = timedelta(days=1)
+app.config["JWT_REFRESH_TOKEN_EXPIRES"] = timedelta(days=1)
 app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('PSQL_URL')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
 
@@ -27,8 +30,10 @@ app.register_blueprint(register)
 app.register_blueprint(post, url_prefix='/post')
 
 @app.route('/')
+@jwt_required()
 def geetings():
-    return jsonify({'greeting': "Hi there!"})
+    curr_user = get_jwt_identity()
+    return jsonify({'currentUser': curr_user})
 
 
 
--- src/auth.py
+++ src/auth.py
@@ -1,8 +1,9 @@
+from xxlimited import new
 from flask import Blueprint, request, jsonify
 from database.database import User
 from constants import ResponseErrorCodes
 from flask_bcrypt import check_password_hash
-from flask_jwt_extended import create_access_token
+from flask_jwt_extended import create_access_token, get_jwt_identity
 
 
 auth = Blueprint('auth', __name__)
@@ -27,8 +28,15 @@ def login():
             return jsonify({'msg': 'Bad username or password', 'err_code': ResponseErrorCodes.USER_NOT_FOUND}), 404
         
         
-        access_token = create_access_token(identity=user.email)
+        access_token = create_access_token(identity=user.username)
         return jsonify({'token': access_token})
 
     else:
         return jsonify({'msg': 'Not valid json.'}), 400
+    
+    
+@auth.route('/refresh-token', methods=['POST'])
+def refresh_token():
+    identity = get_jwt_identity()
+    new_access_token = create_access_token(identity=identity)
+    return jsonify({'refreshedToken': new_access_token}), 201
